81d93efa8dff75567035a83fea17891bd3cfef9a
TekMonksGitHub <rvkapoor@tekmonks.com>
2022-05-17 23:22:27 +0800

    Fix IP parsing in changeHosts.sh

--- changeHosts.sh
+++ changeHosts.sh
@@ -13,7 +13,7 @@ fi
 cp "$MONKSHU_PATH/backend/apps/telemeet/conf/httpd.json" "$MONKSHU_PATH/../monkshu/backend/server/conf/httpd.json"
 for file in $FILES; do
     echo Processing file $file
-    sed -i -r -e "s/https:\/\/[\.0-9]+?/https:\/\/$DOMAIN/g" "$file"
+    sed -i -r -e "s/https:\/\/((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])/https:\/\/$DOMAIN/g" "$file"
     sed -i -r -e "s/\[\"[\.0-9]+?:9090\".*?\]/[\"$DOMAIN:9090\"]/g" "$file"
     sed -i -r -e "s/C\:\/Users\/.+?_privkey.pem/\/etc\/letsencrypt\/live\/$DOMAIN\/privkey.pem/g" "$file"
     sed -i -r -e "s/C\:\/Users\/.+?_fullchain.pem/\/etc\/letsencrypt\/live\/$DOMAIN\/fullchain.pem/g" "$file"
